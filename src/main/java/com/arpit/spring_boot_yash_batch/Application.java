fd5420692e020b5337f38ecf0e547e1e360453be
arpitparekh <arpitparekh9@gmail.com>
2024-08-05 16:06:06 +0530

    final crud operations

---
+++ src/main/java/com/arpit/spring_boot_yash_batch/Application.java
@@ -0,0 +1,13 @@
+package com.arpit.spring_boot_yash_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Spring Boot Yash Batch Application Started...");
+    }
+}
